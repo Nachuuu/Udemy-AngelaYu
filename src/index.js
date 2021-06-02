ac8ac7263f8ad755a696d3a9982348164e778b97
Nachuuu <77378116+Nachuuu@users.noreply.github.com>
2021-06-01 21:12:54 -0500

    prueba prueba

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom";
 const fName = "Edgar";
 const lName = "Gómez";
 
+const fechaActual = new Date();
+const año = fechaActual.getFullYear();
+
 const lucky = 7;
 
 ReactDOM.render(
@@ -16,6 +19,8 @@ ReactDOM.render(
       <li>Miau</li>
     </ul>
     <p>Tu número de la suerte es: {lucky}</p>
+    <p>Creado por : {fName + " " + lName}.</p>
+    <p>Copyright {año}.</p>
   </div>,
   document.getElementById("root")
 );
